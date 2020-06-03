3a2a741d5252974c4da869f80ab13765146e7977
leoshingkung1122 <66315108+leoshingkung1122@users.noreply.github.com>
2020-06-03 23:07:27 +0700

    Added summary variable(Boolean)
    If GPA lower than 2.00 you will be dismissed, but if still be more than or equal 2.00 the condition is safe.

--- Exercise4_Sirapop_T.py
+++ Exercise4_Sirapop_T.py
@@ -13,6 +13,7 @@ comSystemGrade      = C
 comProgramGrade     = F
 numberSubject       = 4
 result = (englishGrade + gBusinessGrade+ comSystemGrade + comProgramGrade)/numberSubject
+summary = (englishGrade + gBusinessGrade+ comSystemGrade + comProgramGrade)/numberSubject >= 2.00
 print("---Your score---")
 print("Foundation English               :",englishGrade)
 print("General Business                 :",gBusinessGrade)
@@ -22,5 +23,11 @@ print("----------------")
 ''' Here is a sum total of your grades and divide by the number of subjects 
 which have enrolled in this semester'''
 print("Average GPA                      :",result)
+print("----------------")
+print("If your GPA is lower than 2.00 you will be dismissed")
+print("Your student condition still is normal :",summary)
+
+
+
 
 
