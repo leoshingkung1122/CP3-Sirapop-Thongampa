b6cb25528f67a28105cb0af6844b88a99aabbb6a
leoshingkung1122 <66315108+leoshingkung1122@users.noreply.github.com>
2020-06-03 19:50:51 +0700

    Added result assign
    result to calculate the accumulated value divided by number of subjects

--- Exercise4_Sirapop_T.py
+++ Exercise4_Sirapop_T.py
@@ -7,6 +7,7 @@ C = 2.00
 D = 1.00
 F = 0.00
 numberSubject = 4
+result = (A+A+B+D)/numberSubject
 print("---Your score---")
 '''Input Grade results after the the comma sign to accumulate and analyze the GPA'''
 print("Foundation English               :",A)
@@ -16,6 +17,6 @@ print("Computer Programming             :",D)
 print("----------------")
 ''' Here is a sum total of your grades and divide by the number of subjects 
 which have enrolled in this semester'''
-print("Average GPA                      :",(A+A+B+D)/numberSubject)
+print("Average GPA                      :",result)
 
 
