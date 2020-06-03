9f477bd62ef6c7b73fdfb9845b5d5832d17ae7b9
leoshingkung1122 <66315108+leoshingkung1122@users.noreply.github.com>
2020-06-03 19:44:52 +0700

    Added new assign variable (numberSubject)
    You can input the number of enrolled subject to the assign variable (numberSubject).

--- Exercise4_Sirapop_T.py
+++ Exercise4_Sirapop_T.py
@@ -6,6 +6,7 @@ B = 3.00
 C = 2.00
 D = 1.00
 F = 0.00
+numberSubject = 4
 print("---Your score---")
 '''Input Grade results after the the comma sign to accumulate and analyze the GPA'''
 print("Foundation English               :",A)
@@ -15,6 +16,6 @@ print("Computer Programming             :",D)
 print("----------------")
 ''' Here is a sum total of your grades and divide by the number of subjects 
 which have enrolled in this semester'''
-print("Average GPA                      :",(A+A+B+D)/4)
+print("Average GPA                      :",(A+A+B+D)/numberSubject)
 
 
