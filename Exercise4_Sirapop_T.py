2869e707291c3090eed7696efdbdfa1f62e3c739
leoshingkung1122 <66315108+leoshingkung1122@users.noreply.github.com>
2020-06-04 01:25:10 +0700

    Big Changing in the code
    I change the system from Grade A,B,C to the float values and I have added the input function of student name and float
    grade to the system.

--- Exercise4_Sirapop_T.py
+++ Exercise4_Sirapop_T.py
@@ -1,28 +1,31 @@
 '''
-This is GPA result for student
+This is GPA calculation Program for student
 '''
-A = 4.00
-B = 3.00
-C = 2.00
-D = 1.00
-F = 0.00
-'''Input Grade results after the the comma sign to accumulate and analyze the GPA'''
-englishGrade        = A
-gBusinessGrade      = B
-comSystemGrade      = C
-comProgramGrade     = F
-numberSubject       = 4
-result = (englishGrade + gBusinessGrade+ comSystemGrade + comProgramGrade)/numberSubject
-summary = (englishGrade + gBusinessGrade+ comSystemGrade + comProgramGrade)/numberSubject >= 2.00
+x = input("Student First name   :")
+y = input("Student Last  name   :")
+print("----------------------------")
+print("For example, Student XXX Grade : 4.00, 3.50, 3.00, 1.50, 0.00")
+print("Note!!! you have to input float values don't input the integer")
+englishGrade    = float(input("Student English    Grade :"))
+gBusinessGrade  = float(input("Student Business   Grade :"))
+comSystemGrade  = float(input("Student ComSystem  Grade :"))
+comProgramGrade = float(input("Student ComProgram Grade :"))
+
+print("----------------------------")
 print("---Your score---")
 print("Foundation English               :",englishGrade)
 print("General Business                 :",gBusinessGrade)
 print("Introduction to Computer Systems :",comSystemGrade)
 print("Computer Programming             :",comProgramGrade)
 print("----------------")
+
 ''' Here is a sum total of your grades and divide by the number of subjects 
 which have enrolled in this semester'''
-print("Average GPA                      :",result)
+numberSubject       = 4
+result = (englishGrade + gBusinessGrade+ comSystemGrade + comProgramGrade)/numberSubject
+summary = (englishGrade + gBusinessGrade+ comSystemGrade + comProgramGrade)/numberSubject >= 2.00
+
+print("Average GPA of (Mr/Mrs)",x,y,"is :",result)
 print("----------------")
 print("If your GPA is lower than 2.00 you will be dismissed")
 print("Your student condition still is normal :",summary)
