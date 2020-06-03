4941bc7ec07d580192d9e6f3781bb152d11a4f84
leoshingkung1122 <66315108+leoshingkung1122@users.noreply.github.com>
2020-06-03 19:08:04 +0700

    Added Exersice 4
    This program is about accumulation and analysis the GPA of students

---
+++ Exercise4_Sirapop_T.py
@@ -0,0 +1,20 @@
+'''
+This is GPA result for student
+'''
+A = 4.00
+B = 3.00
+C = 2.00
+D = 1.00
+F = 0.00
+print("---Your score---")
+'''Input Grade results after the the comma sign to accumulate and analyze the GPA'''
+print("Foundation English               :",A)
+print("General Business                 :",A)
+print("Introduction to Computer Systems :",B)
+print("Computer Programming             :",D)
+print("----------------")
+''' Here is a sum total of your grades and divide by the number of subjects 
+which have enrolled in this semester'''
+print("Average GPA                      :",(A+A+B+D)/4)
+
+
