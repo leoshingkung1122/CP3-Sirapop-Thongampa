47fdceca253f0b48b3bdef037e60cb482aa3e54c
leoshingkung1122 <66315108+leoshingkung1122@users.noreply.github.com>
2020-06-06 07:09:11 +0700

    Pyramid
    input desire value and it will arise the row follow input values in the sahpe of Pyramid

---
+++ Exercise11_Sirapop_T.py
@@ -0,0 +1,49 @@
+input1 = int(input("Please Enter Number : "))
+z = 0
+for x in range(input1):
+    text = ""
+    x = 2*x + 1
+    z += 1
+    a = input1
+    a = input1 - z
+    textS = " "
+    for y in range(x):
+        text += "*"
+    textS = textS * a
+    result = textS + text
+    print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
